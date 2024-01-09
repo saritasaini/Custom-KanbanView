8fa2580e58a1799af7e596853c293c832cb500a6
sarita saini <124562724+saritasaini@users.noreply.github.com>
2024-01-09 17:18:07 +0530

    Create  dragAndDropList.js

---
+++ dragAndDropList.js
@@ -0,0 +1,20 @@
+import { LightningElement,api  } from 'lwc';
+export default class DragAndDropList extends LightningElement {
+    @api records
+    @api stage
+    handleItemDrag(evt){
+        const event = new CustomEvent('listitemdrag', {
+            detail: evt.detail
+        })
+        this.dispatchEvent(event)
+    }
+    handleDragOver(evt){
+        evt.preventDefault()
+    }
+    handleDrop(evt){
+        const event = new CustomEvent('itemdrop', {
+            detail: this.stage
+        })
+        this.dispatchEvent(event)
+    }
+}
